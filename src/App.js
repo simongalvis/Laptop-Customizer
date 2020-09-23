e9b7c51f10d49e667c71942c5a3d2a0fef33cfd5
Simon Galvis <simongalvis9@gmail.com>
2020-09-23 07:44:21 -0400

    import FEATURES data into App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,49 @@ import Summary from './Summary'
 import Total from './Total'
 import './App.css';
 
+const FEATURES = {
+  Processor: [
+    {
+      name: '17th Generation Intel Core HB (7 Core with donut spare)',
+      cost: 700
+    },
+    {
+      name: 'Professor X AMD Fire Breather with sidewinder technology',
+      cost: 1200
+    }
+  ],
+  "Operating System": [
+    {
+      name: 'Ubuntu Linux 16.04',
+      cost: 200
+    },
+    {
+      name: 'Bodhi Linux',
+      cost: 300
+    }
+  ],
+  "Video Card": [
+    {
+      name: 'Toyota Corolla 1.5v',
+      cost: 1150.98
+    },
+    {
+      name: 'Mind mild breeze 2000',
+      cost: 1345
+    }
+  ],
+  Display: [
+    {
+      name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+      cost: 1500
+    },
+    {
+      name: '15.3" HGTV (3840 x 2160) Home makeover edition',
+      cost: 1400
+    },
+  ]
+};
+
 
 class App extends Component {
   state = {
@@ -44,7 +87,7 @@ class App extends Component {
         <main>
           <form className="main__form">
             <h2>Customize your laptop</h2>
-            <Features features={this.props.features} selected={this.state.selected} updateFeature={this.updateFeature} />
+            <Features features={FEATURES} selected={this.state.selected} updateFeature={this.updateFeature} />
           </form>
           <section className="main__summary">
             <h2>Your cart</h2>
